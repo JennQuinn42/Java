9a6c7c8bd5aa10aa23ae5d9b5977a6e0cd0fa239
peter <peter@WIN-MOHAVJCUIO8.local>
2015-07-13 21:43:43 +0100

    XOR demo

---
+++ Strings/src/XorTest.java
@@ -0,0 +1,12 @@
+
+public class XorTest {
+
+	public static void main(String[] args) {
+		System.out.println("Sup People?");
+		boolean test;
+		test = (5<10) ^ (4>20);
+		System.out.println(test);
+
+	}
+
+}
