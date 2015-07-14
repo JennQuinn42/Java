0da436815b5dcaf96e0062fe50172e8d6356e252
JennQuinn42 <jennquinn42@gmail.com>
2015-07-14 10:20:08 +0100

    Update EircodeApp.java
    Instance variable created instead of global.

--- EircodeApp.java
+++ EircodeApp.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class EircodeApp {
 
+	private Scanner scan = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		EircodeApp ea = new EircodeApp();
 		ea.runApp();
 	}
 
 	private void runApp(){
-
-		Scanner scan = new Scanner(System.in);
 
 		//		System.out.println("Hello World!");
 
