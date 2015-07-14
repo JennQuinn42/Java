5a04541b08ae9520e2ac85173d905fb7a6fb91de
Peter Clarke <peterclarkedublin@live.com>
2015-07-14 09:33:48 +0100

    Update EircodeApp.java
    Inserting comments

--- EircodeApp.java
+++ EircodeApp.java
@@ -13,19 +13,26 @@ public class EircodeApp {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("Hello World!");
+//		System.out.println("Hello World!");
 		
+		//Set up relating [] to store the eircode and corresponding addressses
 		String[] eircode = {"D02 Y006","D04 C932","D15 XR2R","D03 RR27","D24 H510"};
 		String[] addresses = {"5 Merrion Square North, Dublin 2","10 Burlington Road, Dublin 4",
 				"Dunsink Observatory, Dunsink Lane, Dublin 15","26 Kincora Road, Clontarf, Dublin 3",
 				"Partas 4A Brookfield Enterprise Centre, Dublin 24"};
 		
+		
+		//ask user and get their required eircode
 		System.out.println("Please Enter Eircode: ");
 		String eircodeInput = scan.nextLine();
-		System.out.println(eircodeInput);
+		System.out.println("You have entered: " + eircodeInput);
+		
+		
+		eircodeInput = eircodeInput.toUpperCase();
 		
 		int elementFound = -1;
 		
+		//check if eircode is in our []
 		for(int i = 0; i < eircode.length; i++){
 			
 			if(eircodeInput.equals(eircode[i])){
@@ -35,15 +42,14 @@ public class EircodeApp {
 			}
 		}
 		
+		//if it's not found
 		if(elementFound == -1){
 			System.out.println("Sorry, eircode not found.");
-		}else{
-			System.out.println(addresses[elementFound]);
+		}else{//if it's found
+			System.out.println(eircodeInput + " is " +addresses[elementFound]);
 		}
-		
-		
-		
-		
+				
+		scan.close();
 		
 	}
 
