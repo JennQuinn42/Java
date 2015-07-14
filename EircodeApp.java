8f46dc1dfc236c6ce7e853e448f0cfec41d15540
Peter Clarke <peterclarkedublin@live.com>
2015-07-14 09:15:48 +0100

    Create EircodeApp.java

---
+++ EircodeApp.java
@@ -0,0 +1,24 @@
+package EircodeApp;
+
+import java.util.Scanner;
+
+public class EircodeApp {
+
+	public static void main(String[] args) {
+		EircodeApp ea = new EircodeApp();
+		ea.runApp();
+	}
+	
+	private void runApp(){
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Hello World!");
+		
+		
+		
+		
+		
+	}
+
+}
